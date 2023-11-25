e7fa663bbd26bb523bba87e2a2171e752bb4b5c1
Furkan Cingöz <138152979+furkanwithcode@users.noreply.github.com>
2023-11-25 13:31:22 +0300

    wtf

--- Africa/Extension/CodableBundleExtension.swift
+++ Africa/Extension/CodableBundleExtension.swift
@@ -9,26 +9,31 @@ import Foundation
 
 
 extension Bundle {
-  func decode(_ file: String) -> [CoverImageModel] {
+  func decode(_ file: String) -> [CoverImage] {
     //1. locate the json file
     guard let url = self.url(forResource: file, withExtension: nil) else {
       fatalError("Failed to locate \(file) in bundle ")
+
     }
 
     //2. creat a property for the data
     guard let data = try? Data(contentsOf: url) else {
       fatalError("Failed to load \(file) from bundle")
+
     }
 
     //3. crate a decoder
     let decoder = JSONDecoder()
 
+
     //4.creat a property for the decode data
-    guard let loaded = try? decoder.decode([CoverImageModel].self, from: data) else {
+    guard let loaded = try? decoder.decode([CoverImage].self, from: data) else {
       fatalError("Failed to decode \(file) from bundle")
+
     }
 
     //5. returtn the ready to use data
     return loaded
+    
   }
 }
